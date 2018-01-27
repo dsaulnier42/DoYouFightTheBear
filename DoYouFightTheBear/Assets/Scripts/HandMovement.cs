988173dc0704c7d2387a0e28c02f91b009dc330b
Daniel Saulnier <DanielSaulnier42@gmail.com>
2018-01-27 07:48:54 -0500

    Basic Mouse Mouse Game

---
+++ DoYouFightTheBear/Assets/Scripts/HandMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour {
+
+    //the layers the ray can hit
+    public LayerMask positionLayer;
+    public LayerMask grabLayer;
+
+    public Vector3 handOffset;
+
+    public GameObject grabable;
+
+    void Update()
+    {
+        FindMousePosition();
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            CheckForGrabable();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            grabable = null;
+        }
+
+        if(grabable != null)
+        {
+            grabable.transform.position = new Vector3(transform.position.x, grabable.transform.position.y, transform.position.z);
+        }
+
+    }
+
+    public void FindMousePosition()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, positionLayer))
+        {
+            if (hit.collider.gameObject.layer == 9)
+                transform.position = hit.point + Vector3.up*.5f;
+            else
+                transform.position = hit.point + handOffset;
+        }
+    }
+
+    public void CheckForGrabable()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,Vector3.down, out hit, Mathf.Infinity, grabLayer))
+        {
+            grabable = hit.collider.gameObject;
+        }
+    }
+}
---
+++ DoYouFightTheBear/Assets/Scripts/Mouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour {
+
+    public Transform pointer;
+
+    Vector3 start;
+
+    private void Start()
+    {
+        start = pointer.transform.position + new Vector3(-3,1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        pointer.transform.position = new Vector3(start.x + transform.position.x * .7f, start.y + transform.position.z * .7f, pointer.transform.position.z);
+	}
+}
