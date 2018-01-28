6d04b6bae338af7fa03538fef2bae04d4b35a31d
Daniel Saulnier <danielsaulnier42@gmail.com>
2018-01-27 21:15:20 -0500

    Coffee Mini Game Done

---
+++ DoYouFightTheBear/Assets/Scripts/CoffeeGame/CoffeManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeManager : MonoBehaviour {
+
+    public float coffeeTimer;
+
+    bool countdown;
+    public TextMesh textMesh;
+
+    private void Update()
+    {
+
+        if (countdown)
+        {
+            coffeeTimer -= Time.deltaTime;
+            textMesh.text = "Coffee \nTime In\n" + Mathf.RoundToInt(coffeeTimer);
+        }
+
+
+        if (coffeeTimer < 0)
+        {
+            textMesh.text = "Coffee";
+            End();
+        }
+    }
+
+    public void SetCountdown(bool cd)
+    {
+        countdown = cd;
+    }
+
+    void End()
+    {
+        Debug.Log("La Bear");
+    }
+
+
+}
---
+++ DoYouFightTheBear/Assets/Scripts/CoffeeGame/CoffeeHand.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeHand : MonoBehaviour {
+
+    public LayerMask positionLayer;
+    public LayerMask grabLayer;
+    public LayerMask coffeLayer;
+
+    public Vector3 handOffset;
+    Vector3 startHandOffset;
+
+    public GameObject grabbed;
+
+    public CoffeManager coffeManager;
+
+    private void Start()
+    {
+        startHandOffset = handOffset;
+    }
+
+    void Update()
+    {
+        FindMousePosition();
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            CheckForGrabable();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            grabbed = null;
+            Dropped();
+        }
+
+        if (grabbed != null)
+        {
+            grabbed.transform.position = new Vector3(transform.position.x + Vector3.left.x, transform.position.y, transform.position.z);
+        }
+
+    }
+
+    void Dropped()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up * 50, Vector3.down, out hit, Mathf.Infinity, coffeLayer))
+        {
+            coffeManager.SetCountdown(true);
+        }
+    }
+
+    public void FindMousePosition()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, positionLayer))
+        {
+            if (hit.collider.gameObject.layer == 9)
+            {
+                transform.position = hit.point + Vector3.up * .5f;
+            }
+            else
+            {
+                transform.position = hit.point + handOffset;
+            }
+
+        }
+    }
+
+    public void CheckForGrabable()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up * 50, Vector3.down, out hit, Mathf.Infinity, grabLayer))
+        {
+            grabbed = hit.collider.gameObject;
+            coffeManager.SetCountdown(false);
+        }
+    }
+}
